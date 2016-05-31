e8b291b3ad37e682625cc8b2329907984382a31a
oom <lnw_oom@hotmail.com>
2016-05-31 10:14:35 +0700

    Upload finish

--- app/src/main/java/com/ssru/lnw_oom/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/com/ssru/lnw_oom/ssrushopbook/SignUpActivity.java
@@ -2,12 +2,88 @@ package com.ssru.lnw_oom.ssrushopbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //  Explicit
+    private EditText nameEditText, surnamEditText,
+            userEditText, passwordEditText;
+    private String nameString, surnameString, userString, passString;
+    private static final String urlUpload = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //  Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnamEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnamEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passString = passwordEditText.getText().toString().trim();
+
+        // Check Space
+        if (nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") || passString.equals("")) {
+            // Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง ค่ะ");
+
+        } else {
+            // No Space
+            uploadNewUser();
+        }
+
+    }   // clickSign
+
+    private void uploadNewUser() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/ssru/php_add_ohmz.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+    }   // uploadNewUser
+
+}   // Main Class
